14c1063bb534cce52fd71bfabf3215803b08c316
Kemylly01 <kemylly.caetano@escola.pr.gov.br>
2025-04-01 12:21:47 -0300

    segundo

--- lianmobile/app/(tabs)/index.tsx
+++ lianmobile/app/(tabs)/index.tsx
@@ -11,38 +11,40 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/galo.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Cock Master</ThemedText>
+        <ThemedText type="title">Cock Fight Master</ThemedText>
         <HelloWave />
       </ThemedView>
+      <br></br>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Saiba mais sobre nossas rinhas</ThemedText>
         <ThemedText>
           Temos o orgulho de ser a primeira e maior rinha de galo autorizada pelo governo federal.
-          Nossas rinhas são organizadas e prepradas para a sua melhor experiência. N
+          Nossas rinhas são organizadas e prepradas para a sua melhor experiência.<br></br>
+          Temos um ambiente pronto para receber você e seu pet amado, respeitando pessoas com deficiencia e
+          outras coisas que possam atrapalhar sua experiencia.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText type="subtitle">Parcerias</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+          Temos o orgulhodedizer que temos nossa parceria com a empresa Urubu do pix, aonde atraves de <br></br>
+          nossas apostas legalizadas você pode investir seu dinheiro da forma mais confiavel:
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <Image
+          source={require('@/assets/images/indiano.gif')}
+          style={styles.Logo}
+        />
+      
+        <br></br>
+      
     </ParallaxScrollView>
+    
   );
 }
 
@@ -57,10 +59,16 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 250,
+    width: 420,
     bottom: 0,
     left: 0,
     position: 'absolute',
   },
+Logo: {
+  height: 400,
+  width: 600,
+  bottom: 0,
+  left: 0,
+},
 });
